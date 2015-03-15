dd89e9c73cdd409b087c065652ad9ff5ffc80c04
mgechev <mgechev@gmail.com>
2015-03-14 17:54:32 -0700

    Bootstrap benchpress

---
+++ benchmarks/sample-benchmark.spec.js
@@ -0,0 +1,44 @@
+describe('deep tree baseline', function() {
+  'use strict';
+
+  var runner, benchpress;
+
+  beforeEach(function () {
+    benchpress = require('benchpress');
+    runner = new benchpress.Runner([
+      //use protractor as Webdriver client
+      benchpress.SeleniumWebDriverAdapter.PROTRACTOR_BINDINGS,
+      //use RegressionSlopeValidator to validate samples
+      benchpress.Validator.bindTo(benchpress.RegressionSlopeValidator),
+      //use 5 samples to calculate slope regression
+      benchpress.bind(benchpress.RegressionSlopeValidator.SAMPLE_SIZE).toValue(5),
+//      //use the script metric to calculate slope regression
+//      benchpress.bind(benchpress.RegressionSlopeValidator.METRIC).toValue('script'),
+      benchpress.bind(benchpress.Options.FORCE_GC).toValue(true)
+    ]);
+  });
+
+  it('should be fast!', function(done) {
+    var depth = 15;
+    //Tells protractor this isn't an Angular 1 application
+    browser.ignoreSynchronization = true;
+    //Load the benchmark, with a tree depth of 9
+    browser.get('http://localhost:8080/tree.html?depth='+depth);
+    /*
+     * Tell benchpress to click the buttons to destroy and re-create the tree for each sample.
+     * Benchpress will log the collected metrics after each sample is collected, and will stop
+     * sampling as soon as the calculated regression slope for last 20 samples is stable.
+     */
+    runner.sample({
+      id: 'deep-tree',
+      execute: function() {
+        /*
+         * Will call querySelector in the browser, but benchpress is smart enough to ignore injected
+         * script.
+         */
+        $('#destroyDom').click();
+        $('#createDom').click();
+      }
+    }).then(done, done.fail);
+  });
+});
