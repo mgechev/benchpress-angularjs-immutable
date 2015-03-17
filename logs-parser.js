2ef2738e87d976ab618ac0bfa1edbb2e2a70e8a8
mgechev <mgechev@gmail.com>
2015-03-17 09:49:11 -0700

    Add legend

--- logs-parser.js
+++ logs-parser.js
@@ -27,6 +27,38 @@ function parseResult(result) {
   };
 }
 
+function getLayout(count, rect) {
+  var entryHeight = 20;
+  var left = rect.left;
+  var result = [];
+  var totalPerEntry = rect.height / count;
+  for (var i = 0; i < count; i += 1) {
+    result.push({
+      top: (totalPerEntry - entryHeight) / 2 + rect.top + totalPerEntry * i,
+      left: left
+    });
+  }
+  return result;
+}
+
+function drawLegendEntry(ctx, data, layout) {
+  ctx.fillStyle = '#444';
+  ctx.font = '13px Verdana';
+  ctx.textAlign = 'left';
+  ctx.fillText(data.label, layout.left + 25, layout.top + 10);
+
+  ctx.fillStyle = data.strokeColor;
+  ctx.rect(layout.left, layout.top, 20, 20);
+  ctx.fill();
+}
+
+function drawLegend(ctx, rect, data) {
+  var layout = getLayout(data.length, rect);
+  data.forEach(function (d, i) {
+    drawLegendEntry(ctx, d, layout[i]);
+  });
+}
+
 function generateChart(labels, dataset) {
   var Canvas = require('canvas');
   var canvas = new Canvas(600, 400);
@@ -37,6 +69,17 @@ function generateChart(labels, dataset) {
     datasets: dataset
   };
   new Chart(ctx).Line(data);
+  var legendSize = {
+    left: 40,
+    top: 12,
+    width: 190,
+    height: 70
+  };
+//  ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+//  ctx.rect(legendSize.left, legendSize.top,
+//      legendSize.width, legendSize.height);
+//  ctx.fill();
+  drawLegend(ctx, legendSize, dataset);
   return canvas;
 }
 
@@ -65,14 +108,14 @@ files.forEach(function (file) {
 });
 
 var colors = [{
-  fillColor: 'rgba(220,220,220,0.2)',
+  fillColor: 'rgba(220,220,220,0.3)',
   strokeColor: 'rgba(220,220,220,1)',
   pointColor: 'rgba(220,220,220,1)',
   pointStrokeColor: '#fff',
   pointHighlightFill: '#fff',
   pointHighlightStroke: 'rgba(220,220,220,1)',
 }, {
-  fillColor: 'rgba(151,187,205,0.2)',
+  fillColor: 'rgba(151,187,205,0.3)',
   strokeColor: 'rgba(151,187,205,1)',
   pointColor: 'rgba(151,187,205,1)',
   pointStrokeColor: '#fff',
@@ -114,6 +157,9 @@ function formatStatistics(perfProp) {
     canvas.toBuffer(function (err, buf) {
       if (err) {
         throw err;
+      }
+      if (!fs.existsSync(CHART_DIR)) {
+        fs.mkdirSync(CHART_DIR);
       }
       fs.writeFileSync('./' + CHART_DIR + '/' + filename, buf);
     });
