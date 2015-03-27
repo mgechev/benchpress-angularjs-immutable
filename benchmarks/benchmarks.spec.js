c0e5ab30de8eeb064137834e0d8c4aa31381fbc9
mgechev <mgechev@gmail.com>
2015-03-26 17:32:50 -0700

    Add basic chart renderer

--- benchmarks/benchmarks.spec.js
+++ benchmarks/benchmarks.spec.js
@@ -3,13 +3,15 @@
 'use strict';
 var runBenchmark = require('./common').runBenchmark;
 
-var dataSizes = [5, 10, 20, 50, 100, 500, 1000, 2000, 5000, 10000, 100000];
-var bindingsCount = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+var dataSizes = [5, 10, 20];//, 50, 100, 500, 1000, 2000, 5000, 10000, 100000];
+var bindingsCount = [5, 10, 20];//, 30, 40, 50, 60, 70, 80, 90, 100];
+
+var PAGE_URL = 'http://localhost:8080';
 
 var benchmarks = [
   function (bindingsCount, dataSize, done) {
     runBenchmark({
-      url: 'http://localhost:8080',
+      url: PAGE_URL,
       description: 'should run immutable creation benchmarks',
       id: 'immutable-' + dataSize + '-' + bindingsCount,
       buttons: ['#immutable-btn', '#clear-btn'],
@@ -24,7 +26,7 @@ var benchmarks = [
   },
   function (bindingsCount, dataSize, done) {
     runBenchmark({
-      url: 'http://localhost:8080',
+      url: PAGE_URL,
       description: 'should run standard creation benchmarks',
       id: 'standard-' + dataSize + '-' + bindingsCount,
       buttons: ['#standard-btn', '#clear-btn'],
@@ -37,39 +39,37 @@ var benchmarks = [
       log: './log'
     }, done);
   },
-  function (bindingsCount, dataSize, done) {
-    runBenchmark({
-      url: 'http://localhost:8080',
-      description: 'should run standard update benchmarks',
-      id: 'standard-' + dataSize + '-' + bindingsCount,
-      buttons: ['#update-standard-btn'],
-      params: {
-        bindingsCount: bindingsCount,
-        dataSize: dataSize,
-        dataType: 'standard',
-        testType: 'update',
-      },
-      log: './log'
-    }, done);
-  },
-  function (bindingsCount, dataSize, done) {
-    runBenchmark({
-      url: 'http://localhost:8080',
-      description: 'should run immutable update benchmarks',
-      id: 'immutable-' + dataSize + '-' + bindingsCount,
-      buttons: ['#update-immutable-btn'],
-      params: {
-        bindingsCount: bindingsCount,
-        dataSize: dataSize,
-        dataType: 'immutable',
-        testType: 'update',
-      },
-      log: './log'
-    }, done);
-  }
+//  function (bindingsCount, dataSize, done) {
+//    runBenchmark({
+//      url: PAGE_URL,
+//      description: 'should run standard update benchmarks',
+//      id: 'standard-' + dataSize + '-' + bindingsCount,
+//      buttons: ['#update-standard-btn'],
+//      params: {
+//        bindingsCount: bindingsCount,
+//        dataSize: dataSize,
+//        dataType: 'standard',
+//        testType: 'update',
+//      },
+//      log: './log'
+//    }, done);
+//  },
+//  function (bindingsCount, dataSize, done) {
+//    runBenchmark({
+//      url: PAGE_URL,
+//      description: 'should run immutable update benchmarks',
+//      id: 'immutable-' + dataSize + '-' + bindingsCount,
+//      buttons: ['#update-immutable-btn'],
+//      params: {
+//        bindingsCount: bindingsCount,
+//        dataSize: dataSize,
+//        dataType: 'immutable',
+//        testType: 'update',
+//      },
+//      log: './log'
+//    }, done);
+//  }
 ];
-
-
 
 var product = [];
 for (var s = 0; s < dataSizes.length; s += 1) {
@@ -82,8 +82,9 @@ for (var s = 0; s < dataSizes.length; s += 1) {
 }
 
 product.forEach(function (data) {
-  it('should work wow', function (done) {
-    benchmarks[0](data.bindingsCount, data.dataSize, done);
+  benchmarks.forEach(function (b) {
+    it('should work wow', function (done) {
+      b(data.bindingsCount, data.dataSize, done);
+    });
   });
 });
-
